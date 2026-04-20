7c69d7bf0aec766c852ec5d991f5b83cc944880e
Nyukaa <annashitikova@gmail.com>
2026-04-20 14:43:11 +0300

    Task12 simulattion a broken deployment

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5001
+
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
   res.send('ok')
 })
+
 app.use(express.static('dist'))
 
 const start = async () => {
