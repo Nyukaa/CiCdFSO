3fb8ccbf98e0ececd6d443d273372c05e315b516
Nyukaa <annashitikova@gmail.com>
2026-04-20 14:31:35 +0300

    Task12  Add health check endpoint in app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001
-
-app.use(express.static('dist'))
+const PORT = process.env.PORT || 5001;
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+app.use(express.static("dist"));
 
 const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
-}
+  await app.listen(PORT);
+  console.log(`server started on port ${PORT}`);
+};
 
-start()
+start();
